6dbd20c9fc62c27aa5b338d320841c3a0ad3a0a5
Andy <telunc@sfu.ca>
2017-06-05 00:30:57 -0700

    Initial commit

---
+++ Source/controller.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controller : MonoBehaviour {
+
+	private bool walk;
+	private Vector3 spawnPosition;
+
+	// Use this for initialization
+	void Start () {
+		spawnPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (walk) {
+			transform.position = transform.position + Camera.main.transform.forward * .5f * Time.deltaTime;
+		}
+
+		if (transform.position.y < -10f) {
+			transform.position = spawnPosition;
+		}
+
+		Ray ray = Camera.main.ViewportPointToRay (new Vector3 (.5f, .5f, 0f));
+		RaycastHit hit;
+
+		if (Physics.Raycast (ray, out hit)) {
+			if (hit.collider.name.Contains ("Plane")) {
+				walk = false;
+			} else {
+				walk = true;
+			}
+		}
+		
+	}
+}
